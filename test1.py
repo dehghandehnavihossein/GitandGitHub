a3ec35e1b3f856fdeb8238fb467e6221fc124ac9
hosseindehghandehnavi <dehghandehnavihossein@gmail.com>
2025-03-16 19:28:33 +0330

    diff

--- test1.py
+++ test1.py
@@ -4,3 +4,7 @@ print('x =',x)
 print("hello world")
 
 print("This is test massage for test branch.")
+
+print("This is test massage for git diff")
+
+print("This is test massage for git diff head")
